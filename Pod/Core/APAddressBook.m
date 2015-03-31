b3fc741752f10eec7ce570ee875beea1fb01c8d9
Benoit Nolens <benoit@truestory.io>
2015-03-31 13:36:58 +0200

    minor refactoring

--- Pod/Core/APAddressBook.m
+++ Pod/Core/APAddressBook.m
@@ -87,89 +87,94 @@ void APAddressBookExternalChangeCallback(ABAddressBookRef addressBookRef, CFDict
     NSArray *descriptors = self.sortDescriptors;
     APContactFilterBlock filterBlock = self.filterBlock;
     
-    ABAddressBookRequestAccessWithCompletion(self.addressBook, ^(bool granted, CFErrorRef errorRef)
-                                             {
-                                                 dispatch_async(self.localQueue, ^
-                                                                {
-                                                                    NSArray *array = nil;
-                                                                    NSError *error = nil;
-                                                                    if (granted)
-                                                                    {
-                                                                        NSMutableSet *included = [[NSMutableSet alloc] init];
-                                                                        CFArrayRef peopleArrayRef = ABAddressBookCopyArrayOfAllPeople(self.addressBook);
-                                                                        NSUInteger contactCount = (NSUInteger)CFArrayGetCount(peopleArrayRef);
-                                                                        NSMutableArray *contacts = [[NSMutableArray alloc] init];
-                                                                        for (NSUInteger i = 0; i < contactCount; i++)
-                                                                        {
-                                                                            ABRecordRef recordRef = CFArrayGetValueAtIndex(peopleArrayRef, i);
-                                                                            APContact *contact = [[APContact alloc] initWithRecordRef:recordRef fieldMask:fieldMask];
-                                                                            
-                                                                            if (!filterBlock || filterBlock(contact))
-                                                                            {
-                                                                                if ([included containsObject:contact.recordID]) {
-                                                                                    continue;
-                                                                                }
-                                                                                
-                                                                                CFArrayRef linkedPeopleArrayRef = ABPersonCopyArrayOfAllLinkedPeople(recordRef);
-                                                                                NSUInteger linkedPeopleCount = CFArrayGetCount(linkedPeopleArrayRef);
-                                                                                for (NSUInteger j = 0; j < linkedPeopleCount; j++)
-                                                                                {
-                                                                                    ABRecordRef linkedRecordRef = CFArrayGetValueAtIndex(linkedPeopleArrayRef, j);
-                                                                                    
-                                                                                    APContact *linkedContact = [[APContact alloc] initWithRecordRef:linkedRecordRef fieldMask:fieldMask];
-                                                                                    NSMutableArray *phones = [contact.phones mutableCopy];
-                                                                                    for (NSString *phone in linkedContact.phones) {
-                                                                                        if ([phones indexOfObject:phone] == NSNotFound) {
-                                                                                            [phones addObject:phone];
-                                                                                        }
-                                                                                    }
-                                                                                    contact.phones = [phones copy];
-                                                                                    
-                                                                                    NSMutableArray *phonesWithLabels = [contact.phonesWithLabels mutableCopy];
-                                                                                    
-                                                                                    for (APPhoneWithLabel *phoneWithLabel in linkedContact.phonesWithLabels) {
-                                                                                        
-                                                                                        BOOL isDup = NO;
-                                                                                        for (APPhoneWithLabel *currentPhonesWithLabel in phonesWithLabels) {
-                                                                                            if ([currentPhonesWithLabel.phone isEqualToString:phoneWithLabel.phone]) {
-                                                                                                isDup = YES;
-                                                                                                break;
-                                                                                            }
-                                                                                        }
-                                                                                        
-                                                                                        if (!isDup) {
-                                                                                            [phonesWithLabels addObject:phoneWithLabel];
-                                                                                        }
-                                                                                    }
-                                                                                    contact.phonesWithLabels = [phonesWithLabels copy];
-                                                                                    
-                                                                                    NSNumber *linkedRecordID = [NSNumber numberWithInteger:ABRecordGetRecordID(linkedRecordRef)];
-                                                                                    [included addObject:linkedRecordID];
-                                                                                }
-                                                                                
-                                                                                [contacts addObject:contact];
-                                                                                
-                                                                                CFRelease(linkedPeopleArrayRef);
-                                                                            }
-                                                                        }
-                                                                        [contacts sortUsingDescriptors:descriptors];
-                                                                        array = contacts.copy;
-                                                                        CFRelease(peopleArrayRef);
-                                                                    }
-                                                                    else if (errorRef)
-                                                                    {
-                                                                        error = (__bridge NSError *)errorRef;
-                                                                    }
-                                                                    
-                                                                    dispatch_async(queue, ^
-                                                                                   {
-                                                                                       if (completionBlock)
-                                                                                       {
-                                                                                           completionBlock(array, error);
-                                                                                       }
-                                                                                   });
-                                                                });
-                                             });
+    ABAddressBookRequestAccessWithCompletion(self.addressBook, ^(bool granted, CFErrorRef errorRef) {
+         dispatch_async(self.localQueue, ^{
+             
+            NSArray *array = nil;
+            NSError *error = nil;
+            if (granted)
+            {
+                NSMutableSet *included = [[NSMutableSet alloc] init];
+                CFArrayRef peopleArrayRef = ABAddressBookCopyArrayOfAllPeople(self.addressBook);
+                NSUInteger contactCount = (NSUInteger)CFArrayGetCount(peopleArrayRef);
+                NSMutableArray *contacts = [[NSMutableArray alloc] init];
+                for (NSUInteger i = 0; i < contactCount; i++)
+                {
+                    ABRecordRef recordRef = CFArrayGetValueAtIndex(peopleArrayRef, i);
+                    APContact *contact = [[APContact alloc] initWithRecordRef:recordRef fieldMask:fieldMask];
+                    
+                    if (!filterBlock || filterBlock(contact))
+                    {
+                        // Is this contact already added?
+                        
+                        if ([included containsObject:contact.recordID]) {
+                            continue;
+                        }
+                        
+                        CFArrayRef linkedPeopleArrayRef = ABPersonCopyArrayOfAllLinkedPeople(recordRef);
+                        NSUInteger linkedPeopleCount = CFArrayGetCount(linkedPeopleArrayRef);
+                        for (NSUInteger j = 0; j < linkedPeopleCount; j++)
+                        {
+                            ABRecordRef linkedRecordRef = CFArrayGetValueAtIndex(linkedPeopleArrayRef, j);
+                            
+                            // Merge phones of this linked contact with contact
+                            
+                            APContact *linkedContact = [[APContact alloc] initWithRecordRef:linkedRecordRef fieldMask:fieldMask];
+                            NSMutableArray *phones = [contact.phones mutableCopy];
+                            for (NSString *phone in linkedContact.phones) {
+                                if ([phones indexOfObject:phone] == NSNotFound) {
+                                    [phones addObject:phone];
+                                }
+                            }
+                            contact.phones = [phones copy];
+                            
+                            
+                            // Merge phonesWithLabels of this linked contact with contact
+                            
+                            NSMutableArray *phonesWithLabels = [contact.phonesWithLabels mutableCopy];
+                            
+                            for (APPhoneWithLabel *phoneWithLabel in linkedContact.phonesWithLabels) {
+                                
+                                BOOL isDup = NO;
+                                for (APPhoneWithLabel *currentPhonesWithLabel in phonesWithLabels) {
+                                    if ([currentPhonesWithLabel.phone isEqualToString:phoneWithLabel.phone]) {
+                                        isDup = YES;
+                                        break;
+                                    }
+                                }
+                                
+                                if (!isDup) {
+                                    [phonesWithLabels addObject:phoneWithLabel];
+                                }
+                            }
+                            contact.phonesWithLabels = [phonesWithLabels copy];
+                            
+                            NSNumber *linkedRecordID = [NSNumber numberWithInteger:ABRecordGetRecordID(linkedRecordRef)];
+                            [included addObject:linkedRecordID];
+                        }
+                        
+                        [contacts addObject:contact];
+                        
+                        CFRelease(linkedPeopleArrayRef);
+                    }
+                }
+                [contacts sortUsingDescriptors:descriptors];
+                array = contacts.copy;
+                CFRelease(peopleArrayRef);
+            }
+            else if (errorRef)
+            {
+                error = (__bridge NSError *)errorRef;
+            }
+            
+            dispatch_async(queue, ^ {
+               if (completionBlock)
+               {
+                   completionBlock(array, error);
+               }
+           });
+        });
+     });
 }
 
 - (void)startObserveChangesWithCallback:(void (^)())callback
